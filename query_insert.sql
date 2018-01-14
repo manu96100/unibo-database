3a2b66f2b15b25303656bea7f8a920788a2cfd25
Unknown <manu96100@live.it>
2018-01-14 16:27:26 +0100

    Query insert

---
+++ query_insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO autori (cognome, nome, data_nascita) VALUES ("Tolkien", "John Ronald Reuel", "1892-01-03");
+INSERT INTO autori (cognome, nome, data_nascita) VALUES ("Rowling", "Joanne", "1965-07-31");
+
+INSERT INTO casa_editrice (nome) VALUES ("Mondadori");
+INSERT INTO casa_editrice (nome) VALUES ("Zanichelli");
+
+INSERT INTO collana (nome, descrizione) VALUES ("Il Signore degli Anelli", "Romanzo fantasy ambientato nella terra di mezzo. Parla di un Hobbit che deve distruggere un anello per portare la pace.");
+INSERT INTO collana (nome, descrizione) VALUES ("Harry Potter", "Ogni libro della serie rappresenta un anno nella vita di Harry dagli undici ai diciassette anni; i libri descrivono ogni anno scolastico trascorso nella scuola di magia e stregoneria di Hogwarts.");
+
+INSERT INTO generi (nome) VALUES ("Fantasy");
+INSERT INTO generi (nome) VALUES ("Giallo");
+INSERT INTO generi (nome) VALUES ("Avventura");
+
+INSERT INTO personale (cognome, nome, data_nascita) VALUES ("Rossi", "Mario", "1980-01-01");
+INSERT INTO personale (cognome, nome, data_nascita) VALUES ("Fabbri", "Matteo", "1996-05-04");
+
+INSERT INTO utenti (cognome, nome, data_nascita, citta, indirizzo) VALUES ("Cortesi", "Emanuele", "1996-09-30", "Bagnacavallo (RA)", "via Mameli 1");
+INSERT INTO utenti (cognome, nome, data_nascita, citta, indirizzo) VALUES ("Capucci", "Fabio", "1996-01-16", "Fusignano (RA)", "via Don Minzoni 17/e");
+

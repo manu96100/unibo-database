7aba86a5c3bbba5cee6465a3e7fddf900cd113fc
Fabio Capucci <fabio.cappuc@gmail.com>
2018-01-16 15:27:25 +0100

    Aggiunto prestiti non restituiti

--- sito/prestito/index.php
+++ sito/prestito/index.php
@@ -1,5 +1,21 @@
 <?php require '../partials/head.php' ?>
 
+<?php
+require "../ConnessioneSQL.php";
+$connessione = new ConnessioneSQL();
+
+$sql = "SELECT prestiti.id AS pres_id, libri.titolo AS titolo, CONCAT(utenti.cognome, ' ',utenti.nome) AS utente, 
+            CONCAT(personale.cognome, ' ', personale.nome) AS person, data_inizio, data_fine, restituito
+        FROM prestiti
+        JOIN libri ON libri.id=prestiti.id_libro
+        JOIN utenti ON utenti.id=prestiti.id_utente
+        JOIN personale ON personale.id=prestiti.id_personale";
+
+if (isset($_GET['id_utente']) && strlen($_GET['id_utente']) > 0) {
+    $sql .= " WHERE prestiti.id_utente=" . $_GET['id_utente'];
+}
+?>
+
 <div class="d-flex justify-content-between align-items-center mb-4">
     <h3>Prestiti</h3>
     <a href="inserisci.php" class="btn btn-primary">Inserisci</a>
@@ -26,27 +42,63 @@
     </thead>
     <tbody>
     <?php
-    require "../ConnessioneSQL.php";
+    $sql_prestiti = $sql . " ORDER BY data_inizio DESC";
 
-    $sql = "SELECT prestiti.id AS pres_id, libri.titolo AS titolo, CONCAT(utenti.cognome, ' ',utenti.nome) AS utente, 
-            CONCAT(personale.cognome, ' ', personale.nome) AS person, data_inizio, data_fine, restituito
-        FROM prestiti
-        JOIN libri ON libri.id=prestiti.id_libro
-        JOIN utenti ON utenti.id=prestiti.id_utente
-        JOIN personale ON personale.id=prestiti.id_personale";
+    $prestiti = $connessione->query($sql_prestiti)->fetch_all(MYSQLI_ASSOC);
 
-    if (isset($_GET['id_utente']) && strlen($_GET['id_utente']) > 0) {
-        $sql .= " WHERE prestiti.id_utente=" . $_GET['id_utente'];
-    }
-
-    $sql .= " ORDER BY restituito";
-
-    $connessione = new ConnessioneSQL();
-    $risultato = $connessione->query($sql)->fetch_all(MYSQLI_ASSOC);
-
-    if (count($risultato) > 0) {
+    if (count($prestiti) > 0) {
         // output data of each row
-        foreach ($risultato as $row) {
+        foreach ($prestiti as $row) {
+            ?>
+            <tr>
+                <td><?php echo $row["pres_id"] ?></td>
+                <td><?php echo $row["titolo"] ?></td>
+                <td><?php echo $row["utente"] ?></td>
+                <td><?php echo $row["person"] ?></td>
+                <td><?php echo $row["data_inizio"] ?></td>
+                <td><?php echo $row["data_fine"] ?></td>
+                <td><?php echo $row["restituito"] ? 'Si' : 'No' ?></td>
+                <td><a href="modifica.php?id=<?php echo $row["pres_id"] ?>">Modifica</a></td>
+            </tr>
+            <?php
+        }
+    } else {
+        ?>
+        <tr>
+            <td rowspan="4">Non ci sono record.</td>
+        </tr>
+        <?php
+    }
+    ?>
+    </tbody>
+</table>
+
+<div class="d-flex justify-content-between align-items-center mb-4 mt-5">
+    <h5>Prestiti non restituiti</h5>
+</div>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th>Cod.</th>
+        <th>Titolo libro</th>
+        <th>Nome utente</th>
+        <th>Nome personale</th>
+        <th>Data inizio</th>
+        <th>Data fine</th>
+        <th>Restituito</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    $sql_non_restituiti = $sql . " AND restituito = 0 ORDER BY data_inizio DESC";
+
+    $prestiti = $connessione->query($sql_non_restituiti)->fetch_all(MYSQLI_ASSOC);
+
+    if (count($prestiti) > 0) {
+        // output data of each row
+        foreach ($prestiti as $row) {
             ?>
             <tr>
                 <td><?php echo $row["pres_id"] ?></td>

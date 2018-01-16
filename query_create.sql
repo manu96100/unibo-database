57e6b5501d6f0c32f67ec2296d572458fc441b98
Unknown <manu96100@live.it>
2018-01-16 11:16:53 +0100

    Aggiunti id

--- query_create.sql
+++ query_create.sql
@@ -38,9 +38,13 @@ CREATE TABLE libri (
   anno_pubblicazione YEAR         NOT NULL,
   quantita           INT          NOT NULL,
   id_editore         INT          NOT NULL,
+  id_collana         INT          NULL     DEFAULT NULL,
   id_espositore      INT          NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (id_editore) REFERENCES casa_editrice (id)
+    ON DELETE RESTRICT
+    ON UPDATE NO ACTION,
+  FOREIGN KEY (id_collana) REFERENCES collane (id)
     ON DELETE RESTRICT
     ON UPDATE NO ACTION,
   FOREIGN KEY (id_espositore) REFERENCES stanze (id)
